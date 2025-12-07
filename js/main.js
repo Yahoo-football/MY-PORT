753e9c01f2daeeb01d7c99d94a953e622d53a43d
Yahoo-football <songchamrern7@gmail.com>
2025-12-07 09:19:08 +0700

    feature

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const  handleMove=e=>{
+    const {currentTarget:target}=e;
+    const rect=target.getBoundingClientRect(),
+        x=e.clientX-rect.left,
+        y=e.clientY-rect.top;
+
+    target.style.setProperty('--mouse-x',`${x}px`)
+    target.style.setProperty('--mouse-y',`${y}px`)
+}
+for (const card of document.querySelectorAll('.card')){
+    card.onmousemove=e=>handleMove(e)
+}
+document.getElementById('card').onmousemove=e=>{
+    for(const card of document.getElementsByClassName('card')){
+        const rect = card.getBoundingClientRect(),
+        x= e.clientX-rect.left,
+        y=e.clientY-rect.top;
+        card.style.setProperty('--mouse-x',`${x}px`)
+        card.style.setProperty('--mouse-y',`${y}px`)
+    }
+}
